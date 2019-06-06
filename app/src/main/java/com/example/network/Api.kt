810082cc8f6be09e9aa9c3f3b329ba452adff98f
zion830 <zion830@naver.com>
2019-06-06 19:33:43 +0900

    Api method parameter 수정

--- app/src/main/java/com/example/network/Api.kt
+++ app/src/main/java/com/example/network/Api.kt
@@ -2,12 +2,11 @@ package com.example.network
 
 import com.example.model.MsgModel
 import com.example.model.MsgResponse
-import io.reactivex.Observable
 import io.reactivex.Single
+import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface Api {
     @POST("/app/hello")
-    fun sendMsg(@Query("msg") msg: MsgModel): Single<MsgResponse>
+    fun sendMsg(@Body msg: MsgModel): Single<MsgResponse>
 }
